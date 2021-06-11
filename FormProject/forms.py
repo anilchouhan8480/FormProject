e917859affd6e95e06b78e3d48c4d26a77a7267e
Anil Chouhan <chauhananil152@gmail.com>
2021-06-11 17:22:46 +0530

    added more features

---
+++ FormProject/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import PasswordChangeForm, AuthenticationForm, UsernameField,PasswordResetForm, SetPasswordForm
+from django.contrib.auth.models import User
+from django.contrib.auth import password_validation
+
+class MyPasswordResetForm(PasswordResetForm):
+		email = forms.EmailField(label=("Email"),max_length=254,
+			widget=forms.EmailInput(attrs={'autocomplete':'email', 'autofocus':True,
+			'class':'form-control'}))
+
+class MySetPasswordForm(SetPasswordForm):
+	new_password1 = forms.CharField(label=("New Password"),
+		strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'autofocus':True,
+			'class':'form-control'}),help_text=password_validation.password_validators_help_text_html())	
+
+
+	new_password2 = forms.CharField(label=("Confirm New Password"),
+		strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'autofocus':True,
+			'class':'form-control'}))
--- FormProject/urls.py
+++ FormProject/urls.py
@@ -1,25 +1,29 @@
-"""FormProject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import MyPasswordResetForm, MySetPasswordForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    # path('accounts/', include('django.contrib.auth.urls')),
     path('appp2/', include('appp2.urls')),
-    path('app3/', include('app3.urls'))
+    path('app3/', include('app3.urls')),
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="passwordreset/password_reset.html",form_class=MyPasswordResetForm),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="passwordreset/password_reset_done.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="passwordreset/password_reset_confirm.html",form_class=MySetPasswordForm), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="passwordreset/password_reset_complete.html"), 
+        name="password_reset_complete"),
+
 ]
